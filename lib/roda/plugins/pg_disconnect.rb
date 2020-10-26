ca0c1f70b52bc4399d5b2cb2239e6812f5baf57b
Jeremy Evans <code@jeremyevans.net>
2020-10-26 15:10:51 -0700

    Update documentation for recent changes to remove chroot and privdrop

--- lib/roda/plugins/pg_disconnect.rb
+++ lib/roda/plugins/pg_disconnect.rb
@@ -8,10 +8,9 @@ class Roda
     # the QUIT signal, allowing Unicorn to finish handling the current request before exiting.
     #
     # This is designed to be used with applications that cannot connect to the database
-    # after application initialization, either because they are using chroot and the database
-    # connection socket is outside the chroot, or because they are using a firewall and access
-    # to the database server is not allowed from the application the process is running as after
-    # privileges are dropped.
+    # after application initialization, either because they are restricting access to the database
+    # socket using unveil, or because they are using a firewall and access to the database server is
+    # not allowed from the application the process is running as after privileges are dropped.
     # 
     # This plugin must be loaded before the roda error_handler plugin, and it assumes usage of the
     # Sequel database library with the postgres adapter and pg driver.
@@ -23,7 +22,7 @@ class Roda
       module InstanceMethods
         # When database connection is lost, kill the worker process, so a new one will be generated.
         # This is necessary because the unix socket used by the database connection is no longer available
-        # once the application is chrooted.
+        # once the application is unveiled or pledged.
         def call
           super
         rescue Sequel::DatabaseDisconnectError, Sequel::DatabaseConnectionError, PG::ConnectionBad
@@ -33,7 +32,7 @@ class Roda
 
         # When database connection is lost, kill the worker process, so a new one will be generated.
         # This is necessary because the unix socket used by the database connection is no longer available
-        # once the application is chrooted.
+        # once the application is unveiled or pledged.
         def _roda_handle_main_route
           super
         rescue Sequel::DatabaseDisconnectError, Sequel::DatabaseConnectionError, PG::ConnectionBad
